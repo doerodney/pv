655cc1e9744d76c98be6645c737a9076ed4f2974
doerodney <doerodney@gmail.com>
2019-07-09 21:30:14 -0700

    Added group by day results.  Added data from 2019-07-09.

--- analysis.R
+++ analysis.R
@@ -7,6 +7,33 @@ df$timestamp = as.POSIXct(df$timestamp, format="%Y-%m-%d %H:%M")
 df$date = as.Date(df$timestamp)
 glimpse(df)
 
+dfDaily <- df %>% 
+          group_by(date) %>% 
+          summarize(
+            systolic = mean(systolic),
+            diastolic = mean(diastolic),
+            pulse = mean(pulse),
+            dosemg = mean(dosemg)
+          )
+glimpse(dfDaily)
+
+# Model the systolic pressure as a function of phlebotomy, hematocrit, and dosage.
+model <- lm(systolic ~ phlebotomy + hematocrit + dosemg, data = dfDaily)
+summary(model)
+
+# Model hematocrit as a function of phlebotomy and dosage.
+model <- lm(hematocrit ~ phlebotomy + dosemg, data = dfDaily)
+summary(model)
+
+# Plot strip charts of blood pressure values:
+ggplot(dfDaily, aes(x=as.POSIXct(date, tz="GMT"), y=systolic, color=dosemg)) +
+  geom_point() +
+  geom_smooth(method="auto", se=TRUE) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  ggtitle('Systolic pressure by time and hydroxyurea dosage') +
+  xlab('date time') +
+  ylab('pressure [mm Hg]') 
+
 # Plot strip charts of blood pressure values:
 ggplot(df, aes(x=as.POSIXct(timestamp, tz="GMT"), y=systolic, color=dosemg)) +
   geom_point() +
